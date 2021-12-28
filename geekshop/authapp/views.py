1f536a0ce825c95106e3862566acbaa9d0b095a5
Nikolos123 <Isponec2>
2021-12-28 21:54:44 +0300

    pipelines

--- geekshop/authapp/views.py
+++ geekshop/authapp/views.py
@@ -2,29 +2,30 @@ from django.contrib import messages, auth
 from django.contrib.auth.views import LoginView, LogoutView
 from django.core.mail import send_mail
 from django.http import HttpResponseRedirect
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
 from django.conf import settings
 # Create your views here.
 from django.urls import reverse, reverse_lazy
 from django.views.generic import FormView, UpdateView
 
-from authapp.forms import UserLoginForm, UserRegisterForm, UserProfilerForm
+from authapp.forms import UserLoginForm, UserRegisterForm, UserProfilerForm, UserProfileEditForm
 from authapp.models import User
 from baskets.models import Basket
 from mainapp.mixin import BaseClassContextMixin, UserDispatchMixin
 
-class LoginListView(LoginView,BaseClassContextMixin):
+
+class LoginListView(LoginView, BaseClassContextMixin):
     template_name = 'authapp/login.html'
     form_class = UserLoginForm
     title = 'GeekShop - Авторизация'
 
-class RegisterListView(FormView,BaseClassContextMixin):
+
+class RegisterListView(FormView, BaseClassContextMixin):
     model = User
     template_name = 'authapp/register.html'
     form_class = UserRegisterForm
     title = 'GeekShop - Регистрация'
     success_url = reverse_lazy('auth:login')
-
 
     def post(self, request, *args, **kwargs):
 
@@ -42,41 +43,40 @@ class RegisterListView(FormView,BaseClassContextMixin):
 
     @staticmethod
     def send_verify_link(user):
-        verify_link = reverse('authapp:verify',args=[user.email,user.activation_key])
+        verify_link = reverse('authapp:verify', args=[user.email, user.activation_key])
         subject = f'Для активации учетной записи {user.username} пройдите по ссылке'
         message = f'Для подтверждения учетной записи {user.username} на портале \n {settings.DOMAIN_NAME}{verify_link}'
-        return send_mail(subject,message,settings.EMAIL_HOST_USER,[user.email],fail_silently=False)
+        return send_mail(subject, message, settings.EMAIL_HOST_USER, [user.email], fail_silently=False)
 
-    def verify(self,email,activate_key):
+    def verify(self, email, activate_key):
         try:
             user = User.objects.get(email=email)
             if user and user.activation_key == activate_key and not user.is_activation_key_expires():
-                user.activation_key =''
+                user.activation_key = ''
                 user.activation_key_expires = None
                 user.is_active = True
                 user.save()
-                auth.login(self,user)
-            return render(self,'authapp/verification.html')
+                auth.login(self, user)
+            return render(self, 'authapp/verification.html')
         except Exception as e:
             return HttpResponseRedirect(reverse('index'))
 
 
-
-
-
-
-
-
-
-
-class ProfileFormView(UpdateView,BaseClassContextMixin,UserDispatchMixin):
+class ProfileFormView(UpdateView, BaseClassContextMixin, UserDispatchMixin):
     template_name = 'authapp/profile.html'
     form_class = UserProfilerForm
     success_url = reverse_lazy('authapp:profile')
     title = 'GeekShop - Профиль'
 
+    def post(self, request, *args, **kwargs):
+        form = UserProfilerForm(data=request.POST, files=request.FILES, instance=request.user)
+        profile_form = UserProfileEditForm(request.POST, instance=request.user.userprofile)
+        if form.is_valid() and profile_form.is_valid():
+            form.save()
+        return redirect(self.success_url)
+
     def form_valid(self, form):
-        messages.set_level(self.request,messages.SUCCESS)
+        messages.set_level(self.request, messages.SUCCESS)
         messages.success(self.request, "Вы успешно зарегистрировались")
         super().form_valid(form)
         return HttpResponseRedirect(self.get_success_url())
@@ -84,10 +84,11 @@ class ProfileFormView(UpdateView,BaseClassContextMixin,UserDispatchMixin):
     def get_object(self, *args, **kwargs):
         return get_object_or_404(User, pk=self.request.user.pk)
 
-    # def get_context_data(self, **kwargs):
-    #     context = super(ProfileFormView, self).get_context_data(**kwargs)
-    #     context['baskets'] = Basket.objects.filter(user=self.request.user)
-    #     return context
+    def get_context_data(self, **kwargs):
+        context = super(ProfileFormView, self).get_context_data(**kwargs)
+        context['profile'] = UserProfileEditForm(instance=self.request.user.userprofile)
+        return context
+
 
 class Logout(LogoutView):
     template_name = "mainapp/index.html"
--- geekshop/geekshop/settings.py
+++ geekshop/geekshop/settings.py
@@ -151,6 +151,7 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 AUTH_USER_MODEL = 'authapp.User'
 LOGIN_URL = '/authapp/login/'
 LOGIN_REDIRECT_URL = '/'
+LOGIN_ERROR_URL = '/'
 
 
 
@@ -180,4 +181,13 @@ SOCIAL_AUTH_VK_OAUTH2_API_VERSION = '5.131'
 SOCIAL_AUTH_VK_OAUTH2_IGNORE_DEFAULT_SCOPE = True
 SOCIAL_AUTH_VK_OAUTH2_SCOPE = ['email']
 
+SOCIAL_AUTH_PIPELINE = (
+    'social_core.pipeline.social_auth.social_details',
+    'social_core.pipeline.social_auth.social_uid',
+    'social_core.pipeline.social_auth.auth_allowed',
+    'social_core.pipeline.social_auth.social_user',
+    'social_core.pipeline.user.create_user',
+    'authapp.pipelines.save_user_profile',
+    'social_core.pipeline.social_auth.associate_user',
+    'social_core.pipeline.social_auth.load_extra_data',)
 
