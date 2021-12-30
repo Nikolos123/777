8b4952eb1a8d3a035cab620c7fec20f6f0de4359
Nikolos123 <Isponec2>
2021-12-30 20:26:13 +0300

    pipelines

--- geekshop/authapp/pipelines.py
+++ geekshop/authapp/pipelines.py
@@ -17,7 +17,7 @@ def save_user_profile(backend,user,response,*args,**kwargs):
 
 
     api_url = urlunparse(('http','api.vk.com','method/users.get',None,
-                          urlencode(OrderedDict(fields=','.join(('bdate','sex','about')),
+                          urlencode(OrderedDict(fields=','.join(('bdate','sex','about','personal','photo_200')),
                                                 access_token =response['access_token'],v=5.131)),None))
 
     resp = requests.get(api_url)
@@ -46,6 +46,18 @@ def save_user_profile(backend,user,response,*args,**kwargs):
     if age < 18:
         user.delete()
         raise AuthForbidden('social_core.backends.vk.VKOAuth2')
+
+
+    if data['photo_200']:
+        photo_link = data['photo_200']
+        photo_response = requests.get(photo_link)
+        path_photo = f'users_image/{user.pk}.jpg'
+        with open(f'media/{path_photo}','wb') as photo:
+            photo.write(photo_response.content)
+        user.image = path_photo
+
+    if data['personal']['langs']:
+        user.userprofile.langs = data['personal']['langs'][0] if len(data['personal']['langs'][0]) > 0 else 'EN'
     user.save()
 
 
